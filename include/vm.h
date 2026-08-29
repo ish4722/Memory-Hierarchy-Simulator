9f2fa33946db45bf9a6a7a9e406fc62a530b41fb
ish4722 <148687005+ish4722@users.noreply.github.com>
2026-08-30 00:05:44 +0530

    Integrate TLB into VM interface

--- include/vm.h
+++ include/vm.h
@@ -7,28 +7,20 @@
 #include <memory>
 #include "physical.h"
 #include "allocator.h"
+#include "tlb.h"
 
-struct VMStats
-{
-    uint64_t page_faults = 0;
-    uint64_t evictions = 0;
-};
+struct VMStats { uint64_t page_faults = 0; uint64_t evictions = 0; };
 
-class VMManager
-{
+class VMManager {
 public:
-    VMManager(PhysicalMemory &pm, Allocator &alloc, size_t page_size = 4096);
+    VMManager(PhysicalMemory &pm, Allocator &alloc, size_t page_size = 4096, size_t tlb_entries = 32);
     std::optional<size_t> translate(int pid, uint64_t va, bool allocate_on_fault = true);
     void set_active_pid(int pid) { active_pid_ = pid; }
     VMStats stats() const { return stats_; }
+    TLBStats tlb_stats() const { return tlb_.stats(); }
+    const TLB &tlb() const { return tlb_; }
 
-    // For visualization
-    struct ProcPT
-    {
-        std::unordered_map<uint64_t, size_t> page_table;
-        std::deque<uint64_t> clock_order;
-        std::unordered_map<uint64_t, bool> ref_bits;
-    };
+    struct ProcPT { std::unordered_map<uint64_t, size_t> page_table; std::deque<uint64_t> clock_order; std::unordered_map<uint64_t, bool> ref_bits; };
     size_t page_size() const { return page_size_; }
     const std::unordered_map<int, ProcPT> &processes() const { return procs_; }
 
@@ -39,6 +31,7 @@ private:
     int active_pid_ = 0;
     std::unordered_map<int, ProcPT> procs_;
     VMStats stats_;
+    TLB tlb_;
     size_t allocate_frame();
-    void evict_frame(ProcPT &pt);
+    bool evict_frame(ProcPT &pt, int pid);
 };
